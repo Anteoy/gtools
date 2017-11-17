1f7d973c48cc38821de62b66689271f336f8072b
anteoy <anteoy@gmail.com>
2017-11-17 18:31:39 +0800

    add test

--- test/arraytostring/arraytostring.go
+++ test/arraytostring/arraytostring.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"encoding/json"
+	"fmt"
 )
 
-func main(){
-	a := []int{1,2,3}
-	b := []string{"1a","2b","3c"}
-	fmt.Println(fmt.Sprintf("%v\n",a))
-	fmt.Println(fmt.Sprintf("%v\n",b))
+func main() {
+	a := []int64{1, 2, 3}
+	b := []string{"1a", "2b", "3c"}
+	fmt.Println(fmt.Sprintf("%v\n", a))
+	fmt.Println(fmt.Sprintf("%v\n", b))
 
-	bb,_ := json.Marshal(a)
-	aa,_ := json.Marshal(b)
+	bb, _ := json.Marshal(a)
+	aa, _ := json.Marshal(b)
 	fmt.Println(string(aa))
 	fmt.Println(string(bb))
 }
