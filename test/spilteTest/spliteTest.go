729f4207ffd41409cf3d3d33794ccbcfc5d88881
anteoy <anteoy@gmail.com>
2017-12-08 18:09:37 +0800

    string(int) bad

--- test/spilteTest/spliteTest.go
+++ test/spilteTest/spliteTest.go
@@ -3,6 +3,7 @@ package main
 import (
 	"strings"
 	"fmt"
+	"strconv"
 )
 
 func main()  {
@@ -10,7 +11,16 @@ func main()  {
 	var aa string = "1班"
 	// len2 cap2 ""
 	b := strings.Split(a,"班")
+	c ,_:= strconv.ParseInt(b[0],10,64)
 	bb := strings.Split(aa,"班")
 	fmt.Printf("%+v\n",b)
 	fmt.Printf("%+v\n",bb)
+	d:= "dsj"+string(3)+string(4)
+	e := string(3)
+	f := fmt.Sprintf("%sg%dc%d","dkjshf",4,3)
+	fmt.Printf("%+v\n",e)
+	fmt.Printf("%+v\n",c)
+	fmt.Printf("%+v\n",d)
+	fmt.Printf("%+v\n",f)
+
 }
