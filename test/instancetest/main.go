527e70deab1f8c7677111bf2e41ebc7e0bcc54c8
anteoy <anteoy@gmail.com>
2018-02-11 10:13:06 +0800

    make arr

--- test/instancetest/main.go
+++ test/instancetest/main.go
@@ -16,4 +16,26 @@ func main(){
 	d := make([]*B,1,1)
 	fmt.Printf("%+v",d[0])
 	d[0] = nil
+
+
+	fmt.Println("=================")
+	arr := make([]*B,1)
+	fmt.Println("len,cap: ",len(arr),cap(arr))
+	b := &B{}
+	arr = append(arr,b)
+	fmt.Println("=================")
+	fmt.Println("len,cap: ",len(arr),cap(arr))
+	//会增加一个nil在里面
+	fmt.Printf("%+v\n",arr)
+	fmt.Println("+++++++++++++++")
+	arr1 := make([]*B,1)
+	arr1[0] = b
+	fmt.Println("len,cap: ",len(arr1),cap(arr1))
+	fmt.Printf("%+v\n",arr1)
+	fmt.Println("+++++++++++++++")
+	arr2 := make([]*B,0)
+	fmt.Println("len,cap: ",len(arr2),cap(arr2))
+	arr2 = append(arr2,b)
+	fmt.Println("len,cap: ",len(arr2),cap(arr2))
+	fmt.Printf("%+v\n",arr2)
 }
