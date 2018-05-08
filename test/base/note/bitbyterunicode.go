fe89d5ac333352bf0e56acf61d21edd1611fbe9d
anteoy <anteoy@gmail.com>
2018-05-08 14:45:16 +0800

    update

--- test/base/note/bitbyterunicode.go
+++ test/base/note/bitbyterunicode.go
@@ -12,4 +12,8 @@ func main() {
 	// rune 为 int32, byte 为 uint8 ,所以1个 rune能包括所有的一个unicode字符
 	fmt.Println(len([]rune(s)))
 
+	fmt.Printf("%T\n", 'a')         // rune是int32的别名
+	var c1, c2 rune = '\u6211', '们' // u6211我的unicode编码
+	fmt.Println(c1 == '我', string(c2) == "\xe4\xbb\xac")
+
 }
