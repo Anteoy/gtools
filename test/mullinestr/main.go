dfa9ff3b5578e4e7749c685d1eb9f4fbd309adea
anteoy <anteoy@gmail.com>
2018-06-28 10:26:50 +0800

    try for "" ``  \n

--- test/mullinestr/main.go
+++ test/mullinestr/main.go
@@ -4,9 +4,14 @@ import "fmt"
 
 func main() {
 	s := fmt.Sprintf(`Hi teacher，please check your headset and microphone and get ready for the class!
-Course details：
+Course details\n：
 Course name：%s
 Teaching Object：%s
 Courseware：%s`, "aa", "bb", "cc")
 	fmt.Println(s)
+	ss := "a\nbc"
+	fmt.Println(ss)
+	// '' only char
+	sss := '\n'
+	fmt.Println(sss)
 }
