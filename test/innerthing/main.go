cec8bef5517ce97f492336e9644136ef8641fc47
anteoy <anteoy@gmail.com>
2018-06-29 11:08:02 +0800

    inner struct slice map

--- test/innerthing/main.go
+++ test/innerthing/main.go
@@ -2,8 +2,15 @@ package main
 
 import "fmt"
 
+type istruct struct {
+	A int
+}
+
 type Message struct {
-	Data map[string]string `json:"data,omitempty"`
+	Data  map[string]string `json:"data,omitempty"`
+	Data1 []int
+	Data2 [2]int
+	Data3 istruct
 }
 
 func main() {
@@ -13,7 +20,26 @@ func main() {
 	a.Data = make(map[string]string, 10)
 	//panic: assignment to entry in nil map
 	a.Data["test"] = "test1"
+	//panic: runtime error: index out of range
+	//a.Data1[0] = 1
+	//0,0
+	fmt.Println(len(a.Data1), cap(a.Data1))
+	a.Data1 = []int{}
+	//0,0
+	fmt.Println(len(a.Data1), cap(a.Data1))
+	//ok 10 10
+	a.Data1 = make([]int, 10, 10)
+	a.Data1[0] = 1
+
+	a.Data2[0] = 1
+
+	a.Data3.A = 1
 	fmt.Println(a)
+
+	//panic: runtime error: index out of range
+	//bb := []int{}
+	//bb[0] = 1
+	//fmt.Println(bb)
 
 	b := new(Message)
 	fmt.Println(b.Data)
@@ -22,4 +48,5 @@ func main() {
 	//panic: assignment to entry in nil map
 	b.Data["test"] = "test1"
 	fmt.Println(b)
+
 }
